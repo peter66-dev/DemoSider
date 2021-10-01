d119142aea6ecb0bb21d389adb839ab5301f4365
phuxiteo3 <vanphuong0606@gmail.com>
2021-10-01 11:24:06 +0700

    first commit

---
+++ src/demo1/Demo1.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demo1;
+
+/**
+ *
+ * @author Dell
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println("Ahihi");
+    }
+   
+}
